9b37c9c637e170ff3d6e2a444e9531240d12dd6c
waka79 <wiraardi79@gmail.com>
2020-03-16 12:34:13 +0700

    add function round robin for implementation scheduling with round robin algorithm

--- main.go
+++ main.go
@@ -2,5 +2,5 @@ package main
 
 func main() {
 	input := newTask()
-	input.fifo()
+	input.RoundRobin()
 }
--- task.go
+++ task.go
@@ -6,7 +6,8 @@ type input [][]int
 
 func newTask() input {
 	task := make(input, 4)
-	task = [][]int{[]int{8, 0, 0, 0}, []int{12, 0, 0, 0}, []int{7, 0, 0, 0}}
+	//bt,at,wt,tat
+	task = [][]int{[]int{10, 0, 0, 0}, []int{5, 0, 0, 0}, []int{8, 0, 0, 0}}
 
 	return task
 }
@@ -22,6 +23,52 @@ func (in input) fifo() {
 		}
 	}
 
+	for i := 0; i < len(in); i++ {
+		in[i][3] = in[i][0] + in[i][2]
+		avwt += in[i][2]
+		avtat += in[i][3]
+	}
+
+	avwt, avtat = in.avTime(avwt, avtat)
+
+	fmt.Println("Average Waiting Time: ", avwt)
+	fmt.Println("Average Turnaround Time: ", avtat)
+	fmt.Println(in)
+}
+
+func (in input) RoundRobin() {
+	var avwt int
+	var avtat int
+	qt := 2
+	proc := len(in)
+	tempBT := []int{}
+	t := 0
+	for i := 0; i < proc; i++ {
+		tempBT = append(tempBT, in[i][0])
+	}
+
+	for {
+		done := true
+
+		for i := 0; i < proc; i++ {
+			if tempBT[i] > 0 {
+				done = false
+				if tempBT[i] > qt {
+					t += qt
+
+					tempBT[i] -= qt
+				} else {
+					t = t + tempBT[i]
+					in[i][2] = t - in[i][0]
+					tempBT[i] = 0
+				}
+			}
+		}
+
+		if done == true {
+			break
+		}
+	}
 	for i := 0; i < len(in); i++ {
 		in[i][3] = in[i][0] + in[i][2]
 		avwt += in[i][2]
