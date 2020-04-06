4a64a61da3cc2435e61007b7b17266787cfa61cd
waka79 <wiraardi79@gmail.com>
2020-04-06 23:24:44 +0700

    add queue and implement algorithm FIFO, Round Robin, and SJF Non-preemptive in main.go

--- main.go
+++ main.go
@@ -1,8 +1,55 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	input := newTask()
-	input = quicksort(input)
-	input.SJF()
-	// input.RoundRobin()
+	queueFIFO := [][]int{}
+	queueRR := [][]int{}
+	queueSJF := [][]int{}
+
+	fifoalgorithm := [][]int{}
+
+	times := 0
+	in := newTask()
+	quicksort(in)
+
+	fmt.Println(in)
+
+	for i := 0; i < len(in); i++ {
+		if in[i][1] == 1 && len(queueFIFO) < 8 && in[i][2] <= times {
+			queueFIFO = append(queueFIFO, in[i])
+			times = times + in[i][3]
+		} else if in[i][1] == 2 || in[i][1] == 1 {
+			queueRR = append(queueRR, in[i])
+		} else if in[i][1] == 3 {
+			queueSJF = append(queueSJF, in[i])
+		}
+
+		if times < in[i][2] || len(queueFIFO) > 7 {
+			fifo(queueFIFO)
+
+			for j := 0; j < len(queueFIFO); j++ {
+				fifoalgorithm = append(fifoalgorithm, queueFIFO[j])
+			}
+			queueFIFO = nil
+		}
+	}
+
+	if queueFIFO != nil {
+		fifo(queueFIFO)
+
+		for j := 0; j < len(queueFIFO); j++ {
+			fifoalgorithm = append(fifoalgorithm, queueFIFO[j])
+		}
+		queueFIFO = nil
+	}
+
+	fmt.Println(fifoalgorithm)
+	fmt.Println(RoundRobin(queueRR))
+	fmt.Println(SJF(queueSJF))
+	// quicksort(in)
+	// in.SJF()
+	// fmt.Println(in)
 }
--- task.go
+++ task.go
@@ -10,36 +10,60 @@ type input [][]int
 func newTask() input {
 	task := make(input, 8)
 	//bt,at,wt,tat
-	//id[0], prior[1], long process[2], bt[3], i/o[4], at[5], wt[6], tat[7]
-	task = [][]int{[]int{1, 1, 40, 20, 5, 0, 0, 0}, []int{2, 1, 60, 10, 10, 0, 0, 0}, []int{3, 3, 20, 5, 2, 5, 0, 0}, []int{4, 2, 30, 15, 5, 5, 0, 0}}
+	//id[0], prior[1], long process[2], bt[3], i/o[4], at[5]
+	task = [][]int{[]int{1, 1, 40, 20, 5, 7},
+		[]int{2, 1, 60, 10, 10, 0},
+		[]int{3, 3, 20, 5, 2, 5},
+		[]int{4, 2, 30, 15, 5, 5},
+		[]int{5, 3, 20, 10, 5, 5},
+		[]int{6, 3, 20, 50, 3, 5},
+		[]int{7, 2, 50, 10, 5, 5},
+		[]int{8, 2, 10, 30, 3, 5},
+		[]int{9, 1, 40, 20, 5, 500}}
 
+	for i := 0; i < len(task); i++ {
+		// Sum long process, bt, and i/o
+		task[i] = append(task[i], task[i][2]+task[i][3]+task[i][4])
+		// add wt and tat
+		task[i] = append(task[i], 0)
+		task[i] = append(task[i], 0)
+		// Removing Long process, bt, and i/o
+		task[i] = append(task[i][:2], task[i][5:]...)
+	}
+
+	// after the process the slice have id[0], prior[1], at[2], bt[3], wt[4], tat[5]
 	return task
 }
 
-func (in input) fifo() {
+func fifo(in input) [][]int {
+
 	var avwt int
 	var avtat int
-	in[0][6] = 0
+	// n := len(in)
+	in[0][4] = 0
 	for i := 1; i < len(in); i++ {
-		in[i][6] = 0
+		in[i][4] = 0
 		for j := 0; j < i; j++ {
-			in[i][6] += in[j][3]
+			in[i][4] += in[j][3]
 		}
 	}
 
 	for i := 0; i < len(in); i++ {
-		in[i][7] = in[i][3] + in[i][6]
-		avwt += in[i][6]
-		avtat += in[i][7]
+		in[i][5] = in[i][3] + in[i][4]
+		avwt += in[i][4]
+		avtat += in[i][5]
 	}
 
-	in.avTime(avwt, avtat)
+	// go avTime(avwt, avtat, n)
+	return in
 }
 
-func (in input) RoundRobin() {
+// RoundRobin ALGORITHM
+func RoundRobin(in input) [][]int {
+
 	var avwt int
 	var avtat int
-	qt := 2
+	qt := 5
 	proc := len(in)
 	tempBT := []int{}
 	t := 0
@@ -59,7 +83,7 @@ func (in input) RoundRobin() {
 					tempBT[i] -= qt
 				} else {
 					t = t + tempBT[i]
-					in[i][6] = t - in[i][3]
+					in[i][4] = t - in[i][3]
 					tempBT[i] = 0
 				}
 			}
@@ -70,19 +94,22 @@ func (in input) RoundRobin() {
 		}
 	}
 	for i := 0; i < len(in); i++ {
-		in[i][7] = in[i][3] + in[i][6]
-		avwt += in[i][6]
-		avtat += in[i][7]
+		in[i][5] = in[i][3] + in[i][4]
+		avwt += in[i][4]
+		avtat += in[i][5]
 	}
 
-	in.avTime(avwt, avtat)
+	// in.avTime(avwt, avtat)
+	return in
 }
 
-func (in input) SJF() {
+// SJF NON-PREEMPTIVE ALGORITHM
+func SJF(in input) [][]int {
+
 	var avwt int
 	var avtat int
 	proc := len(in)
-	// queue := []int{}
+	queue := []int{}
 
 	for i := 0; i < proc; i++ {
 		pos := i
@@ -94,37 +121,38 @@ func (in input) SJF() {
 		in[i], in[pos] = in[pos], in[i]
 	}
 
-	// for i := 0; i < proc; i++ {
-	// 	for j := i + 1; j < proc; j++ {
-	// 		if in[i][3] > in[j][5] {
-	// 			queue = append(queue, j)
-	// 		}
-	// 	}
-	// 	for k := 0; k < len(queue); k++ {
-	// 		for l := k + 1; l < len(queue); l++ {
-	// 			if in[queue[k]][3] > in[queue[l]][3] {
-	// 				in[queue[k]], in[queue[l]] = in[queue[l]], in[queue[k]]
-	// 			}
-	// 		}
-	// 	}
-	// }
+	for i := 0; i < proc; i++ {
+		for j := i + 1; j < proc; j++ {
+			if in[i][3] > in[j][2] {
+				queue = append(queue, j)
+			}
+		}
+		for k := 0; k < len(queue); k++ {
+			for l := k + 1; l < len(queue); l++ {
+				if in[queue[k]][3] > in[queue[l]][3] {
+					in[queue[k]], in[queue[l]] = in[queue[l]], in[queue[k]]
+				}
+			}
+		}
+	}
 
-	in[0][6] = 0
+	in[0][4] = 0
 
 	for i := 1; i < proc; i++ {
-		in[0][6] = 0
+		in[0][4] = 0
 		for j := 0; j < i; j++ {
-			in[i][6] += in[j][3]
+			in[i][4] += in[j][3]
 		}
-		avwt += in[i][6]
+		avwt += in[i][4]
 	}
 
 	for i := 0; i < proc; i++ {
-		in[i][7] = in[i][3] + in[i][6]
-		avtat += in[i][7]
+		in[i][5] = in[i][3] + in[i][4]
+		avtat += in[i][5]
 	}
 
-	in.avTime(avwt, avtat)
+	// in.avTime(avwt, avtat)
+	return in
 }
 
 func quicksort(a [][]int) input {
@@ -139,7 +167,7 @@ func quicksort(a [][]int) input {
 	a[pivot], a[right] = a[right], a[pivot]
 
 	for i := range a {
-		if a[i][5] < a[right][5] {
+		if a[i][2] < a[right][2] {
 			a[left], a[i] = a[i], a[left]
 			left++
 		}
@@ -153,11 +181,10 @@ func quicksort(a [][]int) input {
 	return a
 }
 
-func (in input) avTime(avwt int, avtat int) {
-	avwt /= len(in)
-	avtat /= len(in)
+func avTime(avwt int, avtat int, n int) {
+	avwt /= n
+	avtat /= n
 
 	fmt.Println("Average Waiting Time: ", avwt)
 	fmt.Println("Average Turnaround Time: ", avtat)
-	fmt.Println(in)
 }
